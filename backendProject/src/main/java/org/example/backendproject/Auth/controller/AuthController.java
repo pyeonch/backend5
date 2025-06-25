a6d451d5d0b84f1c49a2dffc27be588450992a20
shineunshik <dmstlrgkxm@naver.com>
2025-06-25 16:38:19 +0900

    20250625 Security 적용 중  7

--- backendProject/src/main/java/org/example/backendproject/Auth/controller/AuthController.java
+++ backendProject/src/main/java/org/example/backendproject/Auth/controller/AuthController.java
@@ -1,6 +1,8 @@
 package org.example.backendproject.Auth.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.example.backendproject.Auth.dto.LoginRequestDTO;
 import org.example.backendproject.Auth.dto.LoginResponseDTO;
@@ -9,10 +11,10 @@ import org.example.backendproject.Auth.service.AuthService;
 import org.example.backendproject.user.dto.UserDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -41,7 +43,35 @@ public class AuthController {
     }
 
 
+    /** 토큰갱신 API */
+    //refresh HTTP 요청 헤더에서 토큰을 추출하고 그 토큰으로 리프레시 토큰을 발급
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshToken(@RequestHeader(value = "Authorization", required = false)
+                                          String authorizationHeader, HttpServletRequest request) {
+        String refreshToken = null;
+        // 1. 쿠키에서 찾기
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    refreshToken = cookie.getValue();
+                }
+            }
+        }
+        // 2. Authorization 헤더 찾기
+        if (refreshToken == null && authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            refreshToken = authorizationHeader.replace("Bearer ", "").trim();
+        }
+        if (refreshToken == null || refreshToken.isEmpty()) {
+            throw new IllegalArgumentException("리프레시 토큰이 없습니다.");
+        }
+        String newAccessToken = authService.refreshToken(refreshToken);
+        //json 객체로 변환하여 front에 내려주기
+        Map<String, String> res = new HashMap<>();
+        res.put("accessToken", newAccessToken);
+        res.put("refreshToken", refreshToken);
 
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
 
 
 }
